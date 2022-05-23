bb33e9e8feb8a694bd727efa783a0c9aeb5e6676
"Aloysius" <"aloycwl@gmail.com">
2022-05-23 20:04:07 +0800

    Done get card rank

--- LargestCard.sol
+++ LargestCard.sol
@@ -16,21 +16,12 @@ contract LargestCard is CS{
     constructor(address a){unchecked{
         (iwag=a,_owner=msg.sender);
     }}
-    /*function DEPOSIT(uint a)external{unchecked{
-        player[msg.sender].balance+=a;
-        IWAC(iwac).BURN(msg.sender,a);
-    }}
-    function WITHDRAW(uint a)external{unchecked{
-        require(player[msg.sender].balance>=a);
-        player[msg.sender].balance-=a;
-        IWAC(iwac).MINT(msg.sender,a);
-    }}*/
     function JOIN(uint a,uint b)public{unchecked{
         if(room[a].players.length<1){
             require(b>9);
             room[a].betSize=b*1*18;
         }
-        require(IWAG(iwag).balanceOf(msg.sender)>=room[a].betSize); //Have money to bet
+        //require(IWAG(iwag).balanceOf(msg.sender)>=room[a].betSize); //Have money to bet
         require(room[a].players.length<20); //Not full
         require(player[msg.sender].room!=a); //Not same room
         require(a>0); //Not reserved room
@@ -55,24 +46,21 @@ contract LargestCard is CS{
         28,31,41,12,42,14,4,32,23,9,17,51,2,5,43,33,20,40,8,49,52,30,22,27,38,35,45,50,26,48,10,11],
         uint(keccak256(abi.encodePacked(block.timestamp))),51,room[a].betSize,room[a].players);
         uint rl=rp.length;
-        Player storage pi;uint i;uint j;uint ran;uint rb;uint highest;
+        uint ran;uint rb;
 
-        for(i=0;i<rl;i++){ //Number of active players in the room
-            IWAG(iwag).BURN(rp[i],bs);
-            (pi=player[rp[i]],rb+=bs); //Generate pool amount
-            uint t;
-            (ran=hash%c,pi.card=table[ran],table[ran]=table[c],hash/=c,c--);
-            //if(j>3)(t%=10,t=t==0?10:t,pi.points=t,highest=t>=highest?t:highest);
-            
+        for(uint i=0;i<rl;i++){ //Number of active players in the room
+            //IWAG(iwag).BURN(rp[i],bs);
+            rb+=bs; //Generate pool amount
+            (ran=hash%c,player[rp[i]].card=table[ran],table[ran]=table[c],hash/=c,c--);
         }
-        c=0;
-        for(i=0;i<rl;i++)if(player[rp[i]].points==highest)c++; //Getting number of winners
+        /*c=0;
+        for(uint i=0;i<rl;i++)if(player[rp[i]].points==highest)c++; //Getting number of winners
         c=rb*9/10/c; //5% each for host and admin 
         IWAG(iwag).MINT(rp[0],rb*1/20);
-        for(i=0;i<rl;i++){ //Distribute tokens
+        for(uint i=0;i<rl;i++){ //Distribute tokens
             if(player[rp[i]].points==highest)IWAG(iwag).MINT(rp[i],c);
             if(IWAG(iwag).balanceOf(rp[i])<bs)LEAVE(a,rp[i]);
-        }
+        }*/
     }}
     function getRoomInfo(uint a)external view returns(address[]memory b,uint[]memory c){unchecked{
         (b=room[a].players,c=new uint[](b.length));
@@ -84,6 +72,11 @@ contract LargestCard is CS{
         return a;
     }}
     function getCardRank()public view returns(uint){
-
+        uint cardNum=player[msg.sender].card;
+        uint cardVal=cardNum%13;
+        if(cardVal==0)cardVal=13;
+        uint mul=4-((cardNum-cardVal)/13);
+        mul=cardVal*4-(4-mul);
+        return cardVal==1?mul+52:mul;
     }
 }

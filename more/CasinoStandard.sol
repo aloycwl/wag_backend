d77be3bc4f4bef3679fff4a0fbeb1d10bdf7a8a3
"Aloysius" <"aloycwl@gmail.com">
2022-06-22 23:35:13 +0800

    Removed interface

--- more/CasinoStandard.sol
+++ more/CasinoStandard.sol
@@ -1,13 +1,12 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
-interface IWAG{
+interface I20{
     function BURN(address,uint)external;
     function MINT(address,uint)external;
     function balanceOf(address)external view returns(uint256);
 }
 contract CS{
-    IWAG internal iwag;
     address internal _owner;
-    constructor(address a){
-        (iwag,_owner)=(IWAG(a),msg.sender);
+    constructor(){
+        _owner=msg.sender;
     }
 }

4f45b604cde7558631a3154944a723af18f366a7
"Aloysius" <"aloycwl@gmail.com">
2022-06-22 22:19:35 +0800

    Mapped 1 more address for room and player

--- LargestCard2.sol
+++ LargestCard2.sol
@@ -11,60 +11,63 @@ contract LargestCard is CS{
         address[]players; //First player automatically is host
         uint betSize;
     }
-    mapping(uint=>Room)public room;
-    mapping(address=>Player)public player;
+    mapping(address=>mapping(uint=>Room))public room;
+    mapping(address=>mapping(address=>Player))public player;
     constructor(address a)CS(a){}
-    function JOIN(uint a,uint b)public{unchecked{
-        if(room[a].players.length<1){
+    function JOIN(address _a,uint a,uint b)public{unchecked{
+        (Room storage r,Player storage p)=(room[_a][a],player[_a][msg.sender]);
+        if(r.players.length<1){
             require(b>9);
-            room[a].betSize=b*1e18;
+            r.betSize=b*1e18;
         }
-        require(iwag.balanceOf(msg.sender)>=room[a].betSize); //Have money to bet
-        require(room[a].players.length<20); //Not full
-        require(player[msg.sender].room!=a); //Not same room
+        require(iwag.balanceOf(msg.sender)>=r.betSize); //Have money to bet
+        require(r.players.length<20); //Not full
+        require(p.room!=a); //Not same room
         require(a>0); //Not reserved room
-        room[a].players.push(msg.sender); //Add a player
-        player[msg.sender].room=a; //In case player disconnect
+        r.players.push(msg.sender); //Add a player
+        p.room=a; //In case player disconnect
     }}
-    function LEAVE(uint a,address b)public{unchecked{ //Only host can kick
-        require(player[msg.sender].room==a||msg.sender==room[player[b].room].players[0]);
-        player[b].room=0;
-        if(room[a].players.length==1)delete room[a]; //Delete room if no more player
+    function LEAVE(address _a,uint a,address b)public{unchecked{ //Only host can kick
+        require(player[_a][msg.sender].room==a||msg.sender==room[_a][player[_a][b].room].players[0]);
+        player[_a][b].room=0;
+        Room storage r=room[_a][a];
+        if(r.players.length==1)delete room[_a][a]; //Delete room if no more player
         else{
             uint c; //Move players up
-            for(uint i=0;i<room[a].players.length;i++)if(room[a].players[i]==b)c=i;
-            (room[a].players[c]=room[a].players[room[a].players.length-1]);
-            room[a].players.pop();
+            for(uint i=0;i<r.players.length;i++)if(r.players[i]==b)c=i;
+            (r.players[c]=r.players[r.players.length-1]);
+            r.players.pop();
         }
     }}
-    function DEAL(uint a)external{unchecked{
-        require(msg.sender==room[a].players[0]); //Host only
+    function DEAL(address _a,uint a)external{unchecked{
+        require(msg.sender==room[_a][a].players[0]); //Host only
         (uint[52]memory table,uint hash,uint c,uint bs,address[]memory rp)=(
         [uint(3),39,19,36,6,24,46,16,29,34,47,1,7,13,15,44,25,18,37,21,
         28,31,41,12,42,14,4,32,23,9,17,51,2,5,43,33,20,40,8,49,52,30,22,27,38,35,45,50,26,48,10,11],
-        uint(keccak256(abi.encodePacked(block.timestamp))),51,room[a].betSize,room[a].players);
+        uint(keccak256(abi.encodePacked(block.timestamp))),51,room[_a][a].betSize,room[_a][a].players);
         uint rl=rp.length;
         uint ran;uint rb;uint highest;
         for(uint i=0;i<rl;i++){ //Number of active players in the room
+            Player storage p=player[_a][rp[i]];
             iwag.BURN(rp[i],bs);
             rb+=bs; //Generate pool amount
-            (ran=hash%c,player[rp[i]].card=table[ran],table[ran]=table[c],hash/=c,c--);
-            uint cardVal=player[rp[i]].card%13;
+            (ran=hash%c,p.card=table[ran],table[ran]=table[c],hash/=c,c--);
+            uint cardVal=p.card%13;
             if(cardVal==0)cardVal=13;
-            uint mul=4-((player[rp[i]].card-cardVal)/13);
-            (mul=cardVal*4-(4-mul),player[rp[i]].points=cardVal==1?mul+52:mul);
-            if(player[rp[i]].points>highest)highest=player[rp[i]].points;
+            uint mul=4-((p.card-cardVal)/13);
+            (mul=cardVal*4-(4-mul),p.points=cardVal==1?mul+52:mul);
+            if(p.points>highest)highest=p.points;
         }
         iwag.MINT(rp[0],rb*1/20); //5% each for host and admin, only 1 winner
         for(uint i=0;i<rl;i++){
-            if(player[rp[i]].points==highest)iwag.MINT(rp[i],rb*9/10);
-            if(iwag.balanceOf(rp[i])<bs)LEAVE(a,rp[i]);
+            if(player[_a][rp[i]].points==highest)iwag.MINT(rp[i],rb*9/10);
+            if(iwag.balanceOf(rp[i])<bs)LEAVE(_a,a,rp[i]);
         }
     }}
-    function getRoomInfo(uint a)external view returns(address[]memory b,uint[]memory c,uint[]memory d,uint e){
+    function getRoomInfo(address _a,uint a)external view returns(address[]memory b,uint[]memory c,uint[]memory d,uint e){
     unchecked{
-        (b=room[a].players,c=new uint[](b.length),d=new uint[](b.length));
-        for(uint i=0;i<b.length;i++)(c[i],d[i])=(player[b[i]].card,player[b[i]].points);
-        e=room[a].betSize;
+        (b=room[_a][a].players,c=new uint[](b.length),d=new uint[](b.length));
+        for(uint i=0;i<b.length;i++)(c[i],d[i])=(player[_a][b[i]].card,player[_a][b[i]].points);
+        e=room[_a][a].betSize;
     }}
 }

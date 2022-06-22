cdac1934fefa1367e7484049382867e335eb0afb
"Aloysius" <"aloycwl@gmail.com">
2022-06-23 00:15:25 +0800

    Changed burn and mint to transferfrom

--- more/CasinoStandard.sol
+++ more/CasinoStandard.sol
@@ -1,8 +1,8 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 interface I20{
-    function BURN(address,uint)external;
-    function MINT(address,uint)external;
     function balanceOf(address)external view returns(uint256);
+    function transferFrom(address,address,uint)external;
+
 }
 contract CS{
     address internal _owner;
